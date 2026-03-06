6644d584e2d06fe4360d67ae06c88e5e9ff2e07c
nonyponymlg-png <nonyponymlg@gmail.com>
2026-03-06 09:44:01 -0600

    Implement selection sort algorithm in Main.java

--- Main.java
+++ Main.java
@@ -18,6 +18,17 @@ public class Main {
     }
 
     public static void selectionSort(int[] array) {
-        
+        for (int i = 0; i < array.length-1; i++) {
+            int min = i;
+            for (int j = i+1; j < array.length; j++) {
+                if(array[min]>array[j]) {
+                    min = j;
+                }
+            }
+            int temp = array[min];
+            array[min] = array[i];
+            array[i] = temp;
+        }
     }
+
 }
